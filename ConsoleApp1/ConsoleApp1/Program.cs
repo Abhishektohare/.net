8e85575f55a78aabe91644d9d6419f7830ad52df
Ramtohae <ramtohare34@gmail.com>
2023-05-15 17:39:32 +0530

    hello

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//First c# Programm
+using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
---
+++ new.c#/new.c#/Form1.cs
@@ -0,0 +1,9 @@
+namespace new.c_;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
